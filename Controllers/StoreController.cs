30f30d4b848aa7eaaddf79f9c990f862192e4e2d
Rajesh <rajesh.it@malabargroup.com>
2015-02-27 15:20:48 +0530

    Version 1 27/02/2015
    Version 1

---
+++ Controllers/StoreController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Music.Models;
+
+namespace Music.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+        MusicStoreentities storeDB = new MusicStoreentities();
+
+        public ActionResult Index()
+             {
+                 var genres = storeDB.Genres.ToList();
+                 return View(genres);
+             }
+
+        public ActionResult Details(int id)
+        {
+            var album = new Album { Title = "Album " + id };
+            return View(album);
+        }
+
+        public ActionResult Browse(string genre)
+        {
+            var genreModel = new Genre { Name = genre };
+            return View(genreModel);
+        }
+    }
+}
---
+++ Models/Album.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Music.Models
+{
+    public class Album
+    {
+        public int AlbumID { get; set; }
+        public string GenreID { get; set; }
+        public int ArtistId { get; set; }
+        public string Title {get; set;}
+        public decimal Price { get; set; }
+        public string AlbumArtUrl { get; set; }
+        public Genre Genre { get; set; }
+        public Artist Artist { get; set; }
+        
+            
+    }
+}
